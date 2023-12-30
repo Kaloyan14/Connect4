2a65a26f9bae87b917ec384c989dd56c2302b966
Kaloyan Marinov <kaloyanam@gmail.com>
2023-12-30 12:29:20 +0200

    RoundedButton now supports JLabels so that images can be attached to the button

--- src/UI/RoundedButton.java
+++ src/UI/RoundedButton.java
@@ -10,16 +10,19 @@ public class RoundedButton extends JButton {
     private int radius;
     private Color background;
     private double factor;
-    private String text;
+    private JLabel lbl;
     public RoundedButton(String text, int radius, Color background) {
-        super(text);
         this.radius = radius;
         this.background = background;
         this.factor = 0.9;
-        this.text = text;
+        this.lbl = new JLabel(text);
         setOpaque(false);
         setBorder(new RoundedBorder(radius, new Color(0x777777)));
-
+        SpringLayout layout = new SpringLayout();
+        setLayout(layout);
+        layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, lbl, 0, SpringLayout.HORIZONTAL_CENTER, this);
+        layout.putConstraint(SpringLayout.VERTICAL_CENTER, lbl, 0, SpringLayout.VERTICAL_CENTER, this);
+        add(lbl);
         addMouseListener(new MouseAdapter() {
             @Override
             public void mouseEntered(MouseEvent e) {
@@ -47,6 +50,47 @@ public class RoundedButton extends JButton {
 
         });
     }
+
+    public RoundedButton(JLabel lbl, int radius, Color background) {
+        this.radius = radius;
+        this.background = background;
+        this.factor = 0.9;
+        this.lbl = lbl;
+        setOpaque(false);
+        setBorder(new RoundedBorder(radius, new Color(0x777777)));
+        SpringLayout layout = new SpringLayout();
+        setLayout(layout);
+        layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, lbl, 0, SpringLayout.HORIZONTAL_CENTER, this);
+        layout.putConstraint(SpringLayout.VERTICAL_CENTER, lbl, 0, SpringLayout.VERTICAL_CENTER, this);
+        add(lbl);
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                int r = background.getRed();
+                int g = background.getGreen();
+                int b = background.getBlue();
+
+                r = (int) (r * factor);
+                g = (int) (g * factor);
+                b = (int) (b * factor);
+
+                setBackground(new Color(r, g, b));
+                repaint();
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                int r = background.getRed();
+                int g = background.getGreen();
+                int b = background.getBlue();
+
+                setBackground(new Color(r, g, b));
+                repaint();
+            }
+
+        });
+    }
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -55,10 +99,6 @@ public class RoundedButton extends JButton {
 
         Graphics2D g2 = (Graphics2D) g.create();
         g2.setColor(getForeground());
-        FontMetrics metrics = g2.getFontMetrics(getFont());
-        int x = (getWidth() - metrics.stringWidth(text)) / 2;
-        int y = ((getHeight() - metrics.getHeight()) / 2) + metrics.getAscent();
-        g2.drawString(text, x, y);
 
         g2.dispose();
     }
@@ -79,6 +119,14 @@ public class RoundedButton extends JButton {
         public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
             g.setColor(color);
             g.drawRoundRect(x, y, width - 1, height - 1, radius, radius);
+        }
+    }
+
+    @Override
+    public void setFont(Font font) {
+        if(lbl != null) {
+            this.lbl.setFont(font);
+            repaint();
         }
     }
 
@@ -104,5 +152,9 @@ public class RoundedButton extends JButton {
 
     public void setFactor(double factor) {
         this.factor = factor;
+    }
+
+    public void setText(JLabel lbl) {
+        this.lbl = lbl;
     }
 }
